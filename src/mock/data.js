ed4a38afda795360fdab0cc3c0066fcfce8c18e9
Tony Thomas <tonythomas504@gmail.com>
2021-02-03 14:06:02 -0500

    this where the chagnes are going to happen

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Tony Thomas',
+  subtitle: 'Software Engineer',
   cta: '',
 };
 
